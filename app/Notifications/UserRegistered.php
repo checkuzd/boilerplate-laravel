32f573bcf2c74255d5ceadcc10ef43a055abb0d5
checkuzd <137487342+checkuzd@users.noreply.github.com>
2024-07-10 12:10:38 +0530

    Registeration Mail added

---
+++ app/Notifications/UserRegistered.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+
+class UserRegistered extends Notification
+{
+    use Queueable;
+
+    private string $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', $this->resetUrl($notifiable))
+            ->line('Thank you for using our application!')
+            ->subject(Lang::get('Welcome User'));
+    }
+
+    protected function resetUrl(mixed $notifiable): string
+    {
+        return url(route('admin.password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+    }
+}
